da7215ec494db9d7c589a12b1bf64e2270bdcd41
mmilner3 <mmilner395@gmail.com>
2022-06-12 07:53:23 -0700

    Continued module work

--- plots.js
+++ plots.js
@@ -1,12 +1,32 @@
-var trace = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-    "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-   };
-   var data = [trace];
-   var layout = {
-    title: "'Pie' Chart",
-   };
-   Plotly.newPlot("plotArea", data, layout);
-   
+function init() {
+  data = [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] 
+  }];
+  Plotly.newPlot("plot", data);
+};
+
+d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+function updatePlotly() {
+  var dropdownMenu = d3.select("#dropdownMenu");
+  var dataset = dropdownMenu.property("value");
+
+  var xData = [1, 2, 3, 4, 5];
+  var yData = [];
+
+  if (dataset === 'dataset1') {
+    yData = [1, 2, 4, 8, 16];
+  };
+
+  if (dataset === 'dataset2') {
+    yData = [1, 10, 100, 1000, 10000];
+  };
+
+  var trace = {
+    x: [xData],
+    y: [yData],
+  };
+  Plotly.restyle("plot", trace);
+};
+
+init();
